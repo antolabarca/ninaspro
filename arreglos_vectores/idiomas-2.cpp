68d934d6d439aeab4a89b5d08dcaf721d76473e2
antolabarca <anto.labarca.s@gmail.com>
2020-08-22 12:27:30 -0400

    identacion

--- arreglos_vectores/idiomas-2.cpp
+++ arreglos_vectores/idiomas-2.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
   int casos, n;
   string idioma1, idioma2;
   cin >> casos;
 
-  for(int i=0; i<casos; i++){
-
+  for(int i=0; i<casos; i++)
+  {
     cin >> n;
     cin >>idioma1;
-    for(int i=1; i<n; i++){
+    for(int i=1; i<n; i++)
+    {
       cin >> idioma2;
-      if(idioma1!=idioma2){
+      if(idioma1!=idioma2)
+      {
         idioma1="ingles";
       }
     }
 
     cout << idioma1 <<endl;
 
-
   }
-
 
   return 0;
 }
